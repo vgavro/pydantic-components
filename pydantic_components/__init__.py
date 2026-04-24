41721f6c3027b2ecc8a3c96768cef44919d662da
Victor Gavro <vgavro@gmail.com>
2026-04-24 20:59:39 +0300

    chore(project): removed pdm dependency, dynamic version added

--- pydantic_components/__init__.py
+++ pydantic_components/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.0.1"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pydantic-components")
+except PackageNotFoundError:
+    __version__ = "0.0.0"  # fallback for local dev
